b7a3b2ca88df42cd244fc54e895633547d4de1cc
Amir Bajracharya <amirbajracharya97@gmail.com>
2026-03-04 17:52:16 +0545

    message

--- core/models.py
+++ core/models.py
@@ -1,2 +1,12 @@
 from django.db import models
 
+class Car(models.Model):
+    brand = models.CharField(max_length=100)
+    model = models.CharField(max_length=100)
+    registration_number = models.CharField(max_length=50, unique=True)
+    rent_per_day = models.DecimalField(max_digits=8, decimal_places=2)
+    available = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f"{self.brand} {self.model}"
+    
