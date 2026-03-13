7d67951fd65399a259b28d9251f62bce694276dc
Amir Bajracharya <amirbajracharya97@gmail.com>
2026-03-13 16:33:49 +0545

    update

--- core/serializers.py
+++ core/serializers.py
@@ -14,4 +14,16 @@ class CustomerSerializer(serializers.ModelSerializer):
 class BookingSerializer(serializers.ModelSerializer):
     class Meta:
         model = Booking
-        fields = "__all__"        
+        fields = "__all__"   
+
+    def create(self, validated_data):
+        car = validated_data['car']
+        start = validated_data['start_date']
+        end = validated_data['end_date']
+
+        days = (end - start).days
+        total = days * car.rent_per_day
+
+        validated_data['total_amount'] = total
+
+        return super().create(validated_data)     
